4ed64d408a246e077dd55c8de8407499a9b6efc0
Mario09122004 <mariorapido5560@hotmail.com>
2024-05-20 11:48:40 -0600

    Programa 6 - Número mayor

---
+++ Ejercicio6.py
@@ -0,0 +1,9 @@
+n="n"
+x = float(input("Escrie un numero: "))
+y = float(input("Escrie otro numero: "))
+if x==y:
+    print("Los 2 numeros son inguales")
+elif x>y:
+    print(f"el primer numero '{x}' es mayor al segundo numero '{y}'")
+else:
+    print(f"el segundo numero '{y}' es mayor al primer numero '{x}'")
