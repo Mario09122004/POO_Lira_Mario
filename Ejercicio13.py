01e4441a9a442bcda68be6f9c306286541c78d26
Mario09122004 <mariorapido5560@hotmail.com>
2024-05-24 17:08:01 -0600

    Programa 13 - for con lista de países

---
+++ Ejercicio13.py
@@ -0,0 +1,3 @@
+Lista = ["Albania","Indonesia","Alemania","Irak","Angola","Irlanda (Eire)","Antigua y Barbuda","Islas Feroe","Argentina","Islas Vírgenes (Norteamericanas)","Aruba"]
+for x in Lista:
+    print(x)
