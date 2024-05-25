5804e316a49488d0f36f1e7659160c0f306bfe23
Mario09122004 <mariorapido5560@hotmail.com>
2024-05-24 18:05:36 -0600

    Programa 3 - imc

--- Ejercicio3.py
+++ Ejercicio3.py
@@ -2,3 +2,4 @@ print("Calcular IMC")
 peso=float(input("Escriba su peso (Kg):"))
 estatura=float(input("Escriba su estatura (M): "))
 print(f"Tu IMC es de: {peso/estatura**2}")
+#######################
