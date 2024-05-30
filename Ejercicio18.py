b9d1db25efcd76ff9a489e8691ead996a88d1d01
Mario09122004 <mariorapido5560@hotmail.com>
2024-05-30 07:54:16 -0600

    Diccionario con descuento

---
+++ Ejercicio18.py
@@ -0,0 +1,16 @@
+
+productos ={
+    "Pan":10,
+    "Manzana":5,
+    "Chetitos":12,
+    "Laptop":7000,
+    "Telefono":4000
+}
+descuento = int(input("De cuanto es el descuento: "))
+for x in productos:
+    productos[x]=productos[x]-(productos[x]*(descuento/100))
+
+print("///////////////////////////////////////////////")
+
+for x in productos:
+    print(f"El descuento de {x} es de {productos[x]}")
