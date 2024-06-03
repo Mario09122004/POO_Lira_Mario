8ee28975b1fe6116da188abf146ae87519f8ee75
Mario09122004 <mariorapido5560@hotmail.com>
2024-06-03 11:32:10 -0600

    programa 21 - Tupla desempaquetada

---
+++ Ejercicio21.py
@@ -0,0 +1,9 @@
+mario = {"Mario", 19, "Español", "C#", "Durango"}
+
+a,b,c,d,e = mario
+
+print(f"Nombre {a}")
+print(f"Edad {b}")
+print(f"Idioma {c}")
+print(f"Lenguaje de programacion que ese aprender {d}")
+print(f"Ciudad {e}")
